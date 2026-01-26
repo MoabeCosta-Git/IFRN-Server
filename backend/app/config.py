a4dd6e9a2666ff22938978090334c32bc5fe9a74
MoabeCosta-Git <a.moabe@escolar.ifrn.edu.br>
2026-01-26 00:34:06 +0000

    chore: adiciona configuração por variáveis de ambiente

--- backend/app/config.py
+++ backend/app/config.py
@@ -3,5 +3,10 @@ import os
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'server.db')}"
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        f"sqlite:///{os.path.join(BASE_DIR, 'server.db')}")
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
