534637a92afae97e2443aecee3d6555740a651c4
MoabeCosta-Git <a.moabe@escolar.ifrn.edu.br>
2026-02-05 01:56:24 +0000

    fix: corrige definição do enum Perfil

--- backend/app/models/enums.py
+++ backend/app/models/enums.py
@@ -9,7 +9,7 @@ class Categoria(enum.Enum):
     preventiva = "Manutenção preventiva"
     defeito = "Defeito/Mal funcionamento"
 
-class Perfil(enum.enum):
+class Perfil(enum.Enum):
     solicitante = "Solicitante"
     prestador = "Prestador"
     admin = "Administrador"
